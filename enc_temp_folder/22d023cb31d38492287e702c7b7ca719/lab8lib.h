37ceb46376be8d4fa574625328e9678d71332b11
eldarguseinli <eldarguseinli@gmail.com>
2019-03-21 23:49:23 +0300

    Stable version

--- enc_temp_folder/22d023cb31d38492287e702c7b7ca719/lab8lib.h
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int findCharNum(char *name1, char *name2);
-int CountEvenOdd(char *name);
-int printFile(char *name);
-int printFileWithCharacters(char *name);
-int fillFile(char *name);
-int fillFileWithCharacters(char *name);
